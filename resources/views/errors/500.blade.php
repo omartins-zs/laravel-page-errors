2abacacd0170498c484b31d61e9dfa9f112c4806
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-03 14:33:09 -0300

    Add Imagem de Error Serve

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -16,11 +16,13 @@
         class='flex items-center justify-center min-h-screen from-gray-800 via-greeen-300 to-blue-500 bg-gradient-to-br'>
         <div class='w-full max-w-lg px-10 py-8 mx-auto bg-white rounded-lg shadow-xl'>
             <div class='max-w-md mx-auto space-y-6'>
-
-                <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+                <div class="py-8 px-4 mx-auto max-w-screen-xl  lg:py-16 lg:px-6">
                     <div class="mx-auto max-w-screen-sm text-center">
+                        <img src="{{ asset('Error_server.png') }}" alt="Ilustração de erro"
+                            class="w-full h-full object-cover" />
                         <h1
                             class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
+
                             500</h1>
                         <p class="mb-4 text-3xl tracking-tight font-bold text-black md:text-4xl">Erro Interno do
                             Servidor.
